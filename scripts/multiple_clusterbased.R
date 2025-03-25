27772213b1ab243792eec666bc435e4a55861a38
Guda Lab <118491177+GudaLab@users.noreply.github.com>
2025-03-25 13:27:39 -0500

    Add files via upload

--- scripts/multiple_clusterbased.R
+++ scripts/multiple_clusterbased.R
@@ -60,7 +60,7 @@ datainput_multiple_clusterbased <- function(index_multiple_clusterbased_input, i
       plots43 <- DotPlot(multiple_sample_clustering, idents=index_m_clusterbased6, features = top_markers_plot, group.by = index_m_clusterbased4, split.by = index_m_clusterbased5, cols=c(rep("blue",150), "white")) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + coord_flip()
     }
     else if(index_m_clusterbased4 == "seurat_clusters" & index_m_clusterbased3 == "VlnPlot") {
-      plots43 <- VlnPlot(multiple_sample_clustering, features = top_markers_plot, ncol = 5, group.by = index_m_clusterbased4, split.by = index_m_clusterbased5)+ theme(legend.position = 'right')
+      plots43 <- VlnPlot(multiple_sample_clustering, idents=index_m_clusterbased6, features = top_markers_plot, ncol = 5, group.by = index_m_clusterbased4, split.by = index_m_clusterbased5)+ theme(legend.position = 'right')
     }
     else if(index_m_clusterbased4 == "seurat_clusters" & index_m_clusterbased3 == "FeaturePlot") {
       Idents(multiple_sample_clustering) <- index_m_clusterbased4
